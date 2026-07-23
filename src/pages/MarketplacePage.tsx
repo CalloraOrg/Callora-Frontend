a0a1e7fba965b611f129f47e181367b653f8c348
AugistineCreates <project.kosiso@gmail.com>
2026-07-23 21:40:15 +0100

    fix: import useCompareStore in MarketplacePage

--- src/pages/MarketplacePage.tsx
+++ src/pages/MarketplacePage.tsx
@@ -20,6 +20,7 @@ import {
 } from "../utils/density";
 import CompareDrawer from "../components/CompareDrawer";
 import RecentlyActiveRail from "../components/RecentlyActiveRail";
+import { useCompareStore } from "../state/compareStore";
 
 export default function MarketplacePage(): JSX.Element {
   const { apis } = useCompareStore();
